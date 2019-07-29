b1ce6caabc1b9ae18f49dd1cb66b87c975f07ca9
LycheeEng <LycheeEng@hotmail.com>
2019-07-29 10:53:39 +0800

    tranalate chat.view

--- 5-network/11-websocket/chat.view/server.js
+++ 5-network/11-websocket/chat.view/server.js
@@ -18,12 +18,12 @@ function accept(req, res) {
 
   if (req.url == '/ws' && req.headers.upgrade &&
       req.headers.upgrade.toLowerCase() == 'websocket' &&
-      // can be Connection: keep-alive, Upgrade
+      // 可以是 Connection: keep-alive, Upgrade
       req.headers.connection.match(/\bupgrade\b/i)) {
     wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
   } else if (req.url == '/') { // index.html
     fs.createReadStream('./index.html').pipe(res);
-  } else { // page not found
+  } else { // 页面不存在
     res.writeHead(404);
     res.end();
   }
@@ -36,7 +36,7 @@ function onSocketConnect(ws) {
   ws.on('message', function(message) {
     log(`message received: ${message}`);
 
-    message = message.slice(0, 50); // max message length will be 50
+    message = message.slice(0, 50); // 最大消息长度为 50
 
     for(let client of clients) {
       client.send(message);
@@ -54,7 +54,7 @@ if (!module.parent) {
   log = console.log;
   http.createServer(accept).listen(8080);
 } else {
-  // to embed into javascript.info
+  // 嵌入 javascript.info
   log = function() {};
   // log = console.log;
   exports.accept = accept;
