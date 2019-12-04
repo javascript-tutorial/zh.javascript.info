fa9d61e0480f1a60b1e48bfab7994826e5e1c4e2
bemself <45781328+bemself@users.noreply.github.com>
2019-12-04 19:38:59 +0800

    arraybuffer: resolve review comment
    Co-Authored-By: LeviDing <imdingxuewen@gmail.com>

--- 4-binary/01-arraybuffer-binary-arrays/01-concat/_js.view/solution.js
+++ 4-binary/01-arraybuffer-binary-arrays/01-concat/_js.view/solution.js
@@ -6,7 +6,7 @@ function concat(arrays) {
 
   let result = new Uint8Array(totalLength);
 
-  // 对于每个数组 - 复制到 result
+  // 对于每个数组 — 复制到 result
   // 下一个数组在前一个后面复制
   let length = 0;
   for(let array of arrays) {
